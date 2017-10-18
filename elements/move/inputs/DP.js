d331fdc93924b2b60c2564930e4639547892a5bb
Ethan H. Jurman <ehj2229@rit.edu>
2017-10-17 21:14:11 -0400

    motions complete

--- elements/move/inputs/DP.js
+++ elements/move/inputs/DP.js
@@ -2,18 +2,11 @@ const html = require('tram-one').html()
 const getTransformStyle = require('../inputModifiers/getTransformStyle');
 
 module.exports = (attrs) => html`
-<svg style=${getTransformStyle(attrs)} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48" height="48" viewBox="0 0 499.99998 500">
-  <defs>
-    <linearGradient id="a">
-      <stop offset="0" stop-color="#ffec00"/>
-      <stop offset="1" stop-color="#ffa301"/>
-    </linearGradient>
-    <linearGradient xlink:href="#a" id="b" gradientUnits="userSpaceOnUse" x1="50.708" y1="568.076" x2="266.651" y2="568.076" gradientTransform="translate(-46.112 450.433) scale(.82483)"/>
-  </defs>
-  <g transform="translate(0 -552.362)" stroke-linejoin="round">
-    <circle cx="275.843" cy="801.544" r="155.455" fill="#bdbdbd" stroke="#747474" stroke-width="13.191"/>
-    <circle cx="276.731" cy="799.767" r="86.302" fill="#727272" stroke="#3f3f3f" stroke-width="10.553"/>
+<g style=${getTransformStyle(attrs)}>
+  <g transform="translate(-26.732 -549.767)" stroke-linejoin="round">
+    <circle r="143.7884" cy="799.7672" cx="276.7315" fill="#bdbdbd" stroke="#747474" stroke-width="12.2008"/>
+    <circle r="78.6612" cy="799.7672" cx="276.7315" fill="#727272" stroke="#3f3f3f" stroke-width="9.6184"/>
   </g>
-  <path d="M272.66 778.754l-190.764 2.638 33.89 38.253 83.133 87.5-105.4-1.76V830.67L5.188 919l88.334 88.335v-58.74H285.1L168.89 821.963l102.89 3.018z" fill="url(#b)" fill-rule="evenodd" stroke="#000" stroke-width="13.397" stroke-linejoin="bevel" transform="translate(0 -552.362)"/>
-</svg>
+  <path d="M228.624 228.4733l184.026 2.5448-32.693 36.902-80.1966 84.4093 101.677-1.6978v-72.076l85.211 85.21-85.2138 85.215v-56.6652H216.6234L328.7287 270.156l-99.2558 2.9115z" fill="#ffec00" fill-rule="evenodd" stroke="#000" stroke-width="12.9227"/>
+</g>
 `
--- elements/move/inputs/HC.js
+++ elements/move/inputs/HC.js
@@ -2,18 +2,18 @@ const html = require('tram-one').html()
 const getTransformStyle = require('../inputModifiers/getTransformStyle');
 
 module.exports = (attrs) => html`
-<svg style=${getTransformStyle(attrs)} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48" height="48" viewBox="0 0 499.99998 500">
-  <defs>
-    <linearGradient id="a">
-      <stop offset="0" stop-color="#ffec00"/>
-      <stop offset="1" stop-color="#ffa301"/>
-    </linearGradient>
-    <linearGradient xlink:href="#a" id="b" gradientUnits="userSpaceOnUse" gradientTransform="rotate(90 -28.494 629.724) scale(.8794)" x1="50.7082" y1="568.0765" x2="152.3653" y2="436.6479"/>
-  </defs>
-  <g transform="translate(0 -552.362)" stroke-linejoin="round">
-    <circle cx="275.8432" cy="801.5438" r="155.4554" fill="#bdbdbd" stroke="#747474" stroke-width="13.1908"/>
-    <circle cx="276.7315" cy="799.7672" r="86.302" fill="#727272" stroke="#3f3f3f" stroke-width="10.5527"/>
+<g style=${getTransformStyle(attrs)} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48" height="48" viewBox="0 0 499.99998 500">
+  <g transform="translate(-26.73 -549.767)" stroke-linejoin="round">
+    <circle r="143.7884" cy="799.7672" cx="276.7315" fill="#bdbdbd" stroke="#747474" stroke-width="12.2008"/>
+    <circle r="78.6612" cy="799.7672" cx="276.7315" fill="#727272" stroke="#3f3f3f" stroke-width="9.6184"/>
   </g>
-  <path d="M120.624 807.086h75.2205l-94.176-94.176-94.176 94.176h57.65c16.1674 255.6318 386.983 288.0904 419.5593 5.8076l.5816-41.648-120.8365 1.2562 2.2145 35.1755 65.8603.6282c-29.8475 217.6396-313.6567 171.6248-311.898-1.2195z" fill="url(#b)" fill-rule="evenodd" stroke="#000" stroke-width="14.283" stroke-linejoin="bevel" transform="translate(0 -552.362)"/>
-</svg>
+  <g fill="#ffec00" fill-rule="evenodd" stroke="#000">
+    <path d="M403.2358 195.3732l66.2913 66.2912H336.9447z" stroke-width="10.4167"/>
+    <path d="M185.05 236.107l.842 39.1003-72.7307 2.7964S84.2427 281.781 76.94 280.435c-2.8955-14.4783-4.3228-39.9884-4.3228-39.9884z" stroke-width="13.5417"/>
+  </g>
+  <g fill="none" stroke-linejoin="round">
+    <path d="M404.1515 260.7793a154.528 154.528 0 0 1-146.064 143.5368A154.528 154.528 0 0 1 97.8196 276.8336" stroke="#000" stroke-width="55.9271"/>
+    <path d="M404.5242 251.0788a154.528 154.528 0 0 1-78.666 133.548 154.528 154.528 0 0 1-154.983-1.8935A154.528 154.528 0 0 1 95.4955 247.303" stroke="#ffec00" stroke-width="31.4961"/>
+  </g>
+</g>
 `
